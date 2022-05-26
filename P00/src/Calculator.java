9822c93c7c7d8774dad9bd69994273f3bf794c9c
21013611 <21013611@10.174.114.132>
2022-05-26 12:35:08 +0800

    Firt push to P00 rep in github

--- P00/src/Calculator.java
+++ P00/src/Calculator.java
@@ -18,7 +18,7 @@ public class Calculator {
 	public int divide(int a, int b) {
 		
 		if (b == 0) {
-			throw new IllegalArgumentException ();
+			throw new IllegalArgumentException ("Division by zero is not supported");
 		} else {
 		return a / b;
 		}
