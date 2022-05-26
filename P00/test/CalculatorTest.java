ae58da799143b59ae208ef7904b0feba4794b718
21013611 <21013611@10.174.114.132>
2022-05-26 12:38:54 +0800

    First push to github

--- P00/test/CalculatorTest.java
+++ P00/test/CalculatorTest.java
@@ -1,6 +1,5 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.fail;
 
 import org.junit.After;
 import org.junit.AfterClass;
